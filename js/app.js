7744d309335e27cf69d8c4bb435d4c9bf069b2a7
KentFletcher <kentfletch09@gmail.com>
2020-03-09 14:12:10 -0700

    intial setup

---
+++ js/app.js
@@ -0,0 +1,18 @@
+'Use Strict';
+
+/*Seattle*/
+var hours = ('6am', '7am' );
+
+var storeOne = {
+  name: 'Seattle',
+  minCust: 23,
+  maxCust: 65,
+  averCookieSales: 6.3,
+
+  randomNumberCust: function(){
+    Math.floor(Math.random() * (10 + 1));
+  }
+
+};
+
+var storeAverage = document.getElementsById('');
