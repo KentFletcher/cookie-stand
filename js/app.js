aba399ba7b2a0342f8e44812a13f7007de0221c1
KentFletcher <kentfletch09@gmail.com>
2020-03-12 14:57:38 -0700

    constructor

--- js/app.js
+++ js/app.js
@@ -1,97 +1,53 @@
 'use strict';
 
-hours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm']
+hours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
-function salmonCookieStore (name,minCust, maxCust, avgCookie) {
+function SalmonCookieStore (name, minCust, maxCust, avgCookie) {
   this.name = name;
   this.minCustomer = minCust;
   this.maxCustomer = maxCust;
   this.avgCookieSales = avgCookie;
- 
-  this.cookiesPerHour = customerHour();
+
+
   this.hoursOpenDay = 14,
-  var cookiesSum = 0;
-  var totalCookies = [];
-  
-  salmonCookieStore.prototype.customerHour = function(){
-    for (var index = 0; index < this.hours.length; index++) {
+  this.cookiesPerHour = customerHour();
+
+}
+
+var cookiesSum = 0;
+var totalCookies = [];
+
+SalmonCookieStore.prototype.customerHour = function(){
+  for (var index = 0; index < this.hours.length; index++) {
     // Getting the random customers per hour
-      var ranCustomerPerHour = Math.floor(Math.random() * (this.maxCustomer - this.minCustomer + 1) + this.minCustomer);
-      /**Average cookies sold per per hour*/
-      var cookiesPerHour = Math.floor(ranCustomerPerHour * this.avgCookieSales);
-      // console.log(cookiesPerHour);
-      totalCookies.push;
-      
-    }
-  },
+    var ranCustomerPerHour = Math.floor(Math.random() * (this.maxCustomer - this.minCustomer + 1) + this.minCustomer);
+    /**Average cookies sold per per hour*/
+    var cookiesPerHour = Math.floor(ranCustomerPerHour * this.avgCookieSales);
+    // console.log(cookiesPerHour);
+    totalCookies.push;
 
-
-  salmonCookieStore.prototype.saleTracker = function () {
-    for (var i = 0; i < this.hours.length; i++){
-      var cookieAndCustomersPerHour = this.randCustomerHour();
-      this.totalCookies.push(cookieAndCustomersPerHour[1]);
-      this.cookiesSum = (this.cookiesSum + cookiesPerHour);
-    }
   }
+},
 
+
+SalmonCookieStore.prototype.saleTracker = function () {
+  for (var i = 0; i < this.hours.length; i++){
+    var cookieAndCustomersPerHour = this.randCustomerHour();
+    this.totalCookies.push(cookieAndCustomersPerHour[1]);
+    this.cookiesSum = (this.cookiesSum + cookiesPerHour);
+  }
 }
 
 
 
+var Seattle = new SalmonCookieStore('Seattle', 23, 65, 6.3);
+var Tokyo = new SalmonCookieStore('Tokyo', 3, 24, 1.2);
+var Dubai = new SalmonCookieStore('Dubai', 11, 38, 3.7);
+var Paris = new SalmonCookieStore('Paris', 20, 38, 2.3);
+var Lima = new SalmonCookieStore('Lima', 2, 16, 4.6);
 
 
 
-/*Seattle Location*/
-
-// var store1 = {
-//   name: 'Seattle',
-//   minCustomer: 23,
-//   maxCustomer: 65,
-//   avgCookieSales: 6.3,
-//   hoursOpenDay: 14,
-//   cookiesSum: 0,
-//   totalCookies: [],
-//   // customersPerHour: [],
-//   hours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
-
-//   /*Method to calculate my random customers and average sales per day and then store in array */
-  
-
-//   /** function to calculate my total cookies per day */
-  
-
-//   /** function to write the relevant data to the DOM, to be rendered to out sales web page*/
-//   write: function () {
-//     /**where the element is going */
-//     var listEl = document.getElementById('list');
-
-//     /**creating element to append to our list */
-//     var itemEl = document.createElement('li');
-//     listEl.appendChild(itemEl);
-
-//     /**store name */
-//     var headEl = document.createElement('p');
-//     itemEl.appendChild(headEl);
-//     headEl.textContent = this.name;
-
-//     var salesList = document.createElement('ul');
-//     listEl.appendChild(salesList);
-
-//     /** loop to create my rows for sales data */
-//     for (var i = 0; i < this.totalCookies.length; i++){
-//       var hourEl = document.createElement('li');
-//       salesList.appendChild(hourEl);
-//       hourEl.textContent = this.hours[i] + ': ' + this.totalCookies[i] + ' cookies';
-//     }
-
-//     var sumEl = document.createElement('li');
-//     salesList.appendChild(sumEl);
-//     sumEl.textContent = 'Total: ' + this. cookiesSum + ' cookies';
-//   }
-// };
-
-// store1.saleTracker();
-// store1.write();
 
 
 // var store2 = {
