7c2d4d923c65a3aa5aefee64a3f664418baad188
KentFletcher <kentfletch09@gmail.com>
2020-03-13 07:36:02 -0700

    object contructor working

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-hours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
 function SalmonCookieStore (name, minCust, maxCust, avgCookie) {
   this.name = name;
@@ -8,30 +8,60 @@ function SalmonCookieStore (name, minCust, maxCust, avgCookie) {
   this.maxCustomer = maxCust;
   this.avgCookieSales = avgCookie;
 
-  var salesPerHour = this.avgSalesPerHour();
-  this.hoursOpenDay = 14,
- 
+  var cookiesPerHour = this.avgSalesPerHour();
+  console.log(cookiesPerHour);
 
+  this.salesPerHour = cookiesPerHour.data;
+  this.totalCookiesSold = cookiesPerHour.sum;
+
+  // var write = this.writeTable();
+  this.hoursOpenDay = 14;
+  // this.cookies = cookiesPerHour.sum;
+  // console.log(totalCookiesSold);
+}
 
 
 SalmonCookieStore.prototype.avgSalesPerHour = function(){
   var totalCookies = [];
+  console.log(totalCookies);
   var cookiesSum = 0;
-
-  for (var index = 0; index < this.hours.length; index++) {
+  for (var index = 0; index < hours.length; index++) {
     // Getting the random customers per hour
     var ranCustomerPerHour = Math.floor(Math.random() * (this.maxCustomer - this.minCustomer + 1) + this.minCustomer);
-    console.log(randCustomerHour);
     /**Average cookies sold per per hour*/
     var cookiesPerHour = Math.floor(ranCustomerPerHour * this.avgCookieSales);
-    this.cookiesSum = (this.cookiesSum + cookiesPerHour);
-    totalCookies.push(this.salesPerHour);
+    console.log(cookiesPerHour);
+    /**Sum of all cookies per day */
+    cookiesSum = (cookiesSum + cookiesPerHour);
+    totalCookies.push(cookiesPerHour);
+    // console.log(cookiesSum);
   }
   return{
     data: totalCookies,
     sum: cookiesSum
   };
-}
+};
+
+/**Write my DOM down here */
+/** function to write the relevant data to the DOM, to be rendered to out sales web page*/
+
+SalmonCookieStore.prototype.writeTable = function(){
+  var tableEl = document.getElementById('table');
+  var rowEl = document.createElement('tr');
+  var cellEl = document.createElement('td');
+  console.log(rowEl);
+  cellEl.textContent = this.name;
+  tableEl.appendChild(rowEl);
+  rowEl.appendChild.cellEl;
+  console.log(cellEl);
+
+  for(var i = 0, i < this.salesPerHour.length; i++){
+    cells = document.createElement('td');
+     rowEl.appendChild(cells);
+    cells.textContent = this.salesPerHour[i];
+  }
+
+};
 
 
 
@@ -40,9 +70,13 @@ var Tokyo = new SalmonCookieStore('Tokyo', 3, 24, 1.2);
 var Dubai = new SalmonCookieStore('Dubai', 11, 38, 3.7);
 var Paris = new SalmonCookieStore('Paris', 20, 38, 2.3);
 var Lima = new SalmonCookieStore('Lima', 2, 16, 4.6);
-console.log(Seattle);
 
-
+Seattle.writeTable();
+Tokyo.writeTable();
+Dubai.writeTable();
+Paris.writeTable();
+Lima.writeTable();
+// console.log(Seattle);
 
 
 // var store2 = {
