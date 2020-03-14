f347d31e3a098a37077bfb48da27c0e278f7fbae
KentFletcher <kentfletch09@gmail.com>
2020-03-13 19:35:01 -0700

    table near complete, just needs header

--- js/app.js
+++ js/app.js
@@ -1,67 +1,102 @@
 'use strict';
 
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+var allStores = [];
 
 function SalmonCookieStore (name, minCust, maxCust, avgCookie) {
   this.name = name;
   this.minCustomer = minCust;
   this.maxCustomer = maxCust;
   this.avgCookieSales = avgCookie;
+  this.cookiesStorePerHour = [];
+  allStores.push(this);
+  // console.log(this.cookiesStorePerHour);
+  this.avgSalesPerHour();
+  // console.log(cookiesPerHour);
 
-  var cookiesPerHour = this.avgSalesPerHour();
-  console.log(cookiesPerHour);
-
-  this.salesPerHour = cookiesPerHour.data;
-  this.totalCookiesSold = cookiesPerHour.sum;
-
-  // var write = this.writeTable();
   this.hoursOpenDay = 14;
-  // this.cookies = cookiesPerHour.sum;
-  // console.log(totalCookiesSold);
+  this.writeTable();
 }
 
 
 SalmonCookieStore.prototype.avgSalesPerHour = function(){
-  var totalCookies = [];
-  console.log(totalCookies);
-  var cookiesSum = 0;
+  /**Loop to get my cookie sales per hour at each store */
   for (var index = 0; index < hours.length; index++) {
     // Getting the random customers per hour
     var ranCustomerPerHour = Math.floor(Math.random() * (this.maxCustomer - this.minCustomer + 1) + this.minCustomer);
     /**Average cookies sold per per hour*/
     var cookiesPerHour = Math.floor(ranCustomerPerHour * this.avgCookieSales);
-    console.log(cookiesPerHour);
-    /**Sum of all cookies per day */
-    cookiesSum = (cookiesSum + cookiesPerHour);
-    totalCookies.push(cookiesPerHour);
-    // console.log(cookiesSum);
+    this.cookiesStorePerHour.push(cookiesPerHour);
+
   }
-  return{
-    data: totalCookies,
-    sum: cookiesSum
-  };
+
 };
 
 /**Write my DOM down here */
-/** function to write the relevant data to the DOM, to be rendered to out sales web page*/
-
+/** prototype to write the name and daily sales data to the DOM, to be rendered to sales web page*/
 SalmonCookieStore.prototype.writeTable = function(){
   var tableEl = document.getElementById('table');
   var rowEl = document.createElement('tr');
   var cellEl = document.createElement('td');
-  console.log(rowEl);
+  // console.log(rowEl);
   cellEl.textContent = this.name;
   tableEl.appendChild(rowEl);
-  rowEl.appendChild.cellEl;
-  console.log(cellEl);
+  rowEl.appendChild(cellEl);
+  // console.log(cellEl);
+  var sumOfCookies = 0;
 
-  for(var i = 0, i < this.salesPerHour.length; i++){
-    cells = document.createElement('td');
-     rowEl.appendChild(cells);
-    cells.textContent = this.salesPerHour[i];
+  /**loop to get my totals for each store */
+  for(var i = 0; i < this.cookiesStorePerHour.length; i++){
+    cellEl = document.createElement('td');
+    cellEl.textContent = this.cookiesStorePerHour[i];
+    rowEl.appendChild(cellEl);
+    sumOfCookies = sumOfCookies + this.cookiesStorePerHour[i];
+    // console.log(sumOfCookies);
+  }
+  cellEl = document.createElement('td');
+  cellEl.textContent = sumOfCookies;
+  rowEl.appendChild(cellEl);
+};
+
+/**Write Header for hours */
+function headerRow(){
+  var headerRow = document.getElementById('table');
+  var rowEl = createElement(tr);
+  headerRow.appendChild(rowEl);
+}
+
+/**function to write my footer row with daily sales data */
+function totalsPerHour(){
+  /**Connect to HTML */
+  var tableElBottomRow = document.getElementById('table');
+  var rowEl = document.createElement('tr');
+  var cellEl = document.createElement('td');
+
+  cellEl.textContent = 'Total';
+  tableElBottomRow.appendChild(rowEl);
+  rowEl.appendChild(cellEl);
+  var dailyAllStores = 0;
+
+  for(var i = 0; i < hours.length; i++){
+    var hourlyTotals = 0;
+
+
+    cellEl = document.createElement('td');
+    for(var j = 0; j < allStores.length; j++){
+      hourlyTotals = hourlyTotals + allStores[j].cookiesStorePerHour[i];
+
+    }
+    cellEl.textContent = hourlyTotals;
+    rowEl.appendChild(cellEl);
+    dailyAllStores = dailyAllStores + hourlyTotals;
   }
 
-};
+  cellEl = document.createElement('td');
+  cellEl.textContent = hourlyTotals;
+  rowEl.appendChild(cellEl);
+  tableElBottomRow.appendChild(rowEl);
+  cellEl.textContent =dailyAllStores;
+}
 
 
 
@@ -71,11 +106,7 @@ var Dubai = new SalmonCookieStore('Dubai', 11, 38, 3.7);
 var Paris = new SalmonCookieStore('Paris', 20, 38, 2.3);
 var Lima = new SalmonCookieStore('Lima', 2, 16, 4.6);
 
-Seattle.writeTable();
-Tokyo.writeTable();
-Dubai.writeTable();
-Paris.writeTable();
-Lima.writeTable();
+totalsPerHour();
 // console.log(Seattle);
 
 
